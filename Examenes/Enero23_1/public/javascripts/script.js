b191b052cde5b006ddb2c53547834f5e7d183873
JuanRomo-dev <juanromo@ucm.es>
2024-06-17 00:08:46 +0200

    a

--- Examenes/Enero23_1/public/javascripts/script.js
+++ Examenes/Enero23_1/public/javascripts/script.js
@@ -45,8 +45,57 @@ document.addEventListener('DOMContentLoaded', function() {
 
         $('#configModal').modal('hide');
     })
-
-   
-
-    
 })
+
+/*
+document.addEventListener('DOMContentLoaded', function() {
+
+    var configurButton = document.getElementById('configur');
+
+    configurButton.addEventListener('click', function() {
+        var configModal = document.getElementById('configModal');
+        configModal.classList.add('show'); // Mostrar el modal (usando CSS .show)
+        configModal.style.display = 'block'; // Asegurarse de que el modal sea visible
+    });
+
+    var configForm = document.getElementById('configForm');
+    configForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevenir el envío del formulario por defecto
+
+        var fontSize = document.getElementById('fontSize').value + 'px';
+        var fontColor = document.getElementById('fontColor').value;
+        var bckgrndColor = document.getElementById('bckgrndColor').value;
+        var bckgrndColorSec1 = document.getElementById('bckgrndColorSec1').value;
+        var bckgrndColorSec2 = document.getElementById('bckgrndColorSec2').value;
+        var bckgrndColorHead = document.getElementById('bckgrndColorHead').value;
+        var bckgrndColorFoot = document.getElementById('bckgrndColorFooter').value;
+        var linkColor = document.getElementById('linkColor').value;
+        var underlineLinks = document.getElementById('underlineLinks').checked ? 'underline' : 'none';
+        var colorNavbar = document.getElementById('colorNavbar').value;
+        var colorLinksNav = document.getElementById('colorLinksNav').value;
+        var underlineNavLinks = document.getElementById('underlineNavLinks').checked ? 'underline' : 'none';
+
+        document.body.style.fontSize = fontSize;
+        document.body.style.color = fontColor;
+        document.body.style.backgroundColor = bckgrndColor;
+        document.getElementById('section1').style.backgroundColor = bckgrndColorSec1;
+        document.getElementById('section2').style.backgroundColor = bckgrndColorSec2;
+        document.getElementById('cabecera').style.backgroundColor = bckgrndColorHead;
+        document.getElementById('pie').style.backgroundColor = bckgrndColorFoot;
+        var links = document.querySelectorAll('a'); // Obtener todos los enlaces
+        links.forEach(function(link) {
+            link.style.color = linkColor; // Aplicar color a los enlaces
+            link.style.textDecoration = underlineLinks; // Aplicar subrayado si es necesario
+        });
+        var navbar = document.getElementById('navbar');
+        navbar.style.backgroundColor = colorNavbar;
+        navbar.style.color = colorLinksNav;
+        navbar.style.textDecoration = underlineNavLinks;
+
+        var configModal = document.getElementById('configModal');
+        configModal.classList.remove('show'); // Ocultar el modal
+        configModal.style.display = 'none'; // Asegurarse de que el modal esté oculto
+    });
+});
+
+*/
